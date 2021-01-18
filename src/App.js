15eaca91bf7c1b458f6ba5a5644a66dbdc8604d7
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-01-18 14:01:01 -0800

    Initial list and game card styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ignite</h1>
       <Home />
     </div>
   );
